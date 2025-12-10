62bad96ce533ac9f1030d7d27deb4841ec0266e6
TY <rcty666@gmail.com>
2025-12-10 23:53:48 +0800

    pbn

--- lib/pet_bottom_nav.dart
+++ lib/pet_bottom_nav.dart
@@ -83,8 +83,8 @@ class PetBottomNavBar extends StatelessWidget {
                 child: GestureDetector(
                   onTap: onScanPressed,
                   child: Container(
-                    width: 70,
-                    height: 70,
+                    width: 68,
+                    height: 68,
                     decoration: BoxDecoration(
                       shape: BoxShape.circle,
                       color: const Color.fromARGB(255, 6, 15, 176),
