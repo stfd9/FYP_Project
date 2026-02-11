306bc430d3ef6cada3dedebe8dcf5787319e7183
TY <rcty666@gmail.com>
2026-02-11 15:39:27 +0800

    configuration update

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -17,6 +17,17 @@ subprojects {
 }
 subprojects {
     project.evaluationDependsOn(":app")
+    
+    // Set Java 11 for all subprojects to avoid deprecation warnings
+    afterEvaluate {
+        if (project.hasProperty("android")) {
+            val android = project.extensions.getByName("android") as com.android.build.gradle.BaseExtension
+            android.compileOptions.apply {
+                sourceCompatibility = JavaVersion.VERSION_11
+                targetCompatibility = JavaVersion.VERSION_11
+            }
+        }
+    }
 }
 
 tasks.register<Delete>("clean") {
